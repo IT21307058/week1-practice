36fa80a59555a3cda2fd58c06ed664a08e9aa4f2
Bhanuka Lakshitha Dayananda <99331889+IT21307058@users.noreply.github.com>
2025-12-31 06:50:09 +0530

    payment type changes commited

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -5,10 +5,9 @@ const upload = multer();
 const postController = require('../controllers/postController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
+// hi
 router.post('/upload', upload.single('file'), postController.upload);
 router.get('/', postController.getPosts);
 router.delete('/:id', authMiddleware, postController.deletePost);
 router.get('/test', postController.testEndpoint);
-router.get('/test1', postController.testEndpoint1);
-
 module.exports = router;
