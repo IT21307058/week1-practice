e11a0bbb1321a827cc09e3b25f7761219aa95440
Bhanuka Lakshitha Dayananda <99331889+IT21307058@users.noreply.github.com>
2025-12-31 15:04:20 +0530

    Add missing contract file

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -14,6 +14,6 @@ router.get('/test1', postController.testEndpoint1);
 router.get('/test2', postController.testEndpoint2);
 router.get('/test3', postController.testEndpoint3);
 router.get('/test4', postController.testEndpoint4);
-router.get('/test4', postController.testEndpoint5);
+router.get('/test5', postController.testEndpoint5);
 
 module.exports = router;
